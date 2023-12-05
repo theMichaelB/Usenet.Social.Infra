a29a355626835500370bcbfc296afc9dd3b26364
themichaelb <michael@azured.io>
2023-12-05 04:40:15 +0000

    added depends

--- main.tf
+++ main.tf
@@ -135,6 +135,7 @@ data "azurerm_resource_group" "dns" {
 data "azurerm_public_ip" "this" {
   name                = "usenet-social-pip"
   resource_group_name = data.azurerm_resource_group.dns.name
+  depends_on = [ azurerm_linux_virtual_machine.this ]
 }
 
 data "azurerm_dns_zone" "this" {
